95e5fa7edc6a928543d1ea71d5812a424deaf9df
rodilconcha <159867279+rodilconcha@users.noreply.github.com>
2024-07-17 04:24:06 -0400

    Add files via upload
    student database add

---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleApp9;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class SchoolDbContext : DbContext
+        public DbSet<Student> Students {  get; set; }
+    
+    protected override void OnConfiguring( DbContextOptionsBuilder optionsBuilder)
+    { 
+        optionsBuilder.UseSqlServer(("Server= ")
+    }
+}
+using (var context = new SchoolDbContext))
+        {
+     context.Database.EnsureCreated();
+    var grade1=new Grade { GradeName= "1st Grade"}
+    var student1=new Student { FirstName= "Rodil",LastName ="Concha", Grade =grade1 };
+
+    context.Students.Add(student1);
+    context.SaveChanges();
+}
--- Program.cs
+++ Program.cs
@@ -1,37 +1,18 @@
 ﻿
 using System;
-
-class Employee : IQuittable
+public class Student
 {
-    public int Id { get; set; }
+    public int StudentId { get; set; }
     public string FirstName { get; set; }
-    public string LastName { get; set; }
-
-    // Other properties and methods specific to Employee
-
-    public void Quit()
-    {
-        Console.WriteLine($"{FirstName} {LastName} has Quit their job.");
-        // Implement any other logic related to quitting here.
-    }
+    public LastName { get; set; }
+    public int GradeId { get; set; }
+    public Grade Grade { get; set; }
 }
 
-class Program
+public class Grade
 {
-    static void Main()
-    {
-        // Create an Employee object
-        Employee emp = new Employee
-        {
-            Id = 123,
-            FirstName = "Robert",
-            LastName = "Williams"
-        };
-
-        // Polymorphism: Create an IQuittable reference pointing to the Employee object
-        IQuittable quittableEmp = emp;
-
-        // Call the Quit() method
-        quittableEmp.Quit();
+    public int GradeId { get; set; }
+    public string GradeName { get; set; }
+    public IList<Student>Students{ get; set; 
     }
-}
+   
